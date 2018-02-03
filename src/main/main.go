3c0e98884572e28b5b3d3ced9a819727395e740a
hekai <kai.he02@ele.me>
2018-02-03 15:35:13 +0800

    basic type demo

--- src/main/main.go
+++ src/main/main.go
@@ -1,8 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+
+type (
+	byte int8
+	rune int32
+)
+
 
 func main() {
-	fmt.Println("Hello Golang")
+
+	a, _, c, d := 1, 3, 4, 5
+	fmt.Println(a)
+	fmt.Println(c)
+	fmt.Println(d)
+
+	fmt.Println("=====================")
+
+	var a1 float32 = 1.1
+	fmt.Println(a1)
+	b := int(a1)
+	fmt.Println(b)
+
+	fmt.Println("==========数字转String===========")
+	str := 5
+	b1 := strconv.Itoa(str)
+	fmt.Println(b1)
+
 
 }
