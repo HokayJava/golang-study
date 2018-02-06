29e4a9eabbfdcf8dcfceac4b0f3df359129b0558
hekai <kai.he02@ele.me>
2018-02-06 13:00:44 +0800

    bubblesort

--- src/main/bubble_sort.go
+++ src/main/bubble_sort.go
@@ -24,4 +24,43 @@ func main() {
 	g, h := 1, 2
 	i := [...]*int{&g, &h}
 	fmt.Println(i)
+
+	j := [2] int{1, 2}
+	k := [2] int{1, 2}
+	fmt.Println(j == k)
+
+	m := [10]int{}
+	m[1] = 2
+	fmt.Println(m)
+	l := new([10]int)
+	fmt.Println(l)
+
+	fmt.Println("======================")
+	n := [2][3]int{
+		{1, 2, 3},
+		{4, 5, 6}}
+	fmt.Println(n)
+
+	fmt.Println("======================")
+	o := [...][3]int{
+		{1, 2, 3},
+		{4, 5, 6}}
+	fmt.Println(o)
+
+	fmt.Println("===========bubblesort===========")
+
+	q := [...]int{1, 2, 0, 5, 3, 6}
+	fmt.Println(q)
+	for i := 0; i < len(q); i++ {
+		for j := i + 1; j < len(q); j++ {
+			if q[i] < q[j] {
+				temp := q[i]
+				q[i] = q[j]
+				q[j] = temp
+			}
+		}
+
+	}
+	fmt.Println(q)
+
 }
